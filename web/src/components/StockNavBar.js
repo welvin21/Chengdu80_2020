279c2d3475e0d65516baffba89f0c27a92f8f6ce
agarwalmanya <manya18ag@gmail.com>
2020-10-27 12:05:43 +0800

    web: search stocks

--- web/src/components/StockNavBar.js
+++ web/src/components/StockNavBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Input, AutoComplete } from "antd";
 import { Link, useRouteMatch, useLocation } from "react-router-dom";
 
 const { Content, Sider } = Layout;
@@ -9,6 +9,11 @@ const stockItems = [
     ticker: "AAPL",
     name: "Apple",
     route: "aapl",
+  },
+  {
+    ticker: "AAPL",
+    name: "Apples",
+    route: "aap",
   },
   {
     ticker: "GOOG",
@@ -22,13 +27,29 @@ const stockItems = [
   },
 ];
 
+const options = [];
+stockItems.map((item) => options.push({ value: item.name }));
+
 export const StockNavBar = ({ children }) => {
   const match = useRouteMatch();
   const [collapsed, setCollapsed] = useState(false);
+  const [filteredStocks, setFilteredStocks] = useState(stockItems);
+  const [stockOptions, setStockOptions] = useState([]);
+  const handleSearch = (value) => {
+    let res = [];
+    if (!value || value.indexOf("@") >= 0) {
+      res = stockItems
+    } else {
+      res = stockItems.filter(
+        (item) => item.name.toUpperCase().indexOf(value.toUpperCase()) !== -1
+      );
+      console.log(res);
+    }
+    setFilteredStocks(res);
+  };
   const onCollapse = (collapsed) => {
     setCollapsed(collapsed);
   };
-
   const location = useLocation();
   const pathname = location.pathname;
   const currentSelection = pathname.split("/")[2];
@@ -41,8 +62,23 @@ export const StockNavBar = ({ children }) => {
         width={300}
       >
         <div className="logo" />
-        <Menu theme="dark" defaultSelectedKeys={[currentSelection]} mode="inline">
-          {stockItems.map((item) => (
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={[currentSelection]}
+          mode="inline"
+        >
+          <AutoComplete
+            style={{ width: 200, padding: "5px", margin: "auto" }}
+            options={options}
+            placeholder="Search Stock"
+            onSearch={handleSearch}
+            filterOption={(inputValue, option) =>
+              option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !==
+              -1
+            }
+            open={false}
+          ></AutoComplete>
+          {filteredStocks.map((item) => (
             <Menu.Item key={item.route}>
               <Link to={`${match.url}/${item.route}`}>{item.name}</Link>
             </Menu.Item>
