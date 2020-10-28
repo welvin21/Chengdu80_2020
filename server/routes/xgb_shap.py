ba74e78670a988680c7e7e0a1fdd0ca8d3c400c1
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-10-28 18:14:33 +0800

    Enable get returns data endpoint (#19)
    * server: created endpoint for predicting stocks with xgboost
    
    * server: Deleted unused estimators
    
    * server: updated classifier
    
    * server: updated column names to be more descriptive
    
    * server: added industry_info folder
    
    * Freeze pip dependencies into requirements.txt
    
    * Move industry info data into database
    
    * Create get returns data API
    
    Co-authored-by: Diab Haque <diabhaque@gmail.com>

--- server/routes/xgb_shap.py
+++ server/routes/xgb_shap.py
@@ -12,6 +12,10 @@ from xgboost import plot_importance, XGBClassifier
 import matplotlib.pyplot as plt
 import shap
 import time
+import ta
+from ta.volatility import BollingerBands
+from ta.trend import ADXIndicator
+from ta.momentum import UltimateOscillator, RSIIndicator, StochasticOscillator
 
 transaction_data = pd.read_csv('../datasets/transaction_data.tsv', sep='\t')
 
@@ -30,22 +34,53 @@ def get_stock_predictions():
     date = -1
 
     try:
+        # industry_df = pd.read_csv(f'/content/industry_info/{industry}_returns.csv')
+        # industry_df.set_index('date', inplace=True, drop=True)
+
         df = stocks[ticker].copy()
         df['return'] = df['PRC'].pct_change()
         df.drop( columns = ['COMNAM'], inplace = True)
         df.set_index('date', inplace=True, drop=True)
         df['1_day_return'] = df['return'].shift(-1)
         df['1_day_return'] = np.where(df['1_day_return'] > 0, 1, 0)
+
+        ewma = pd.Series.ewm
+
+        df['Fast_Exp_Moving_Avg'] = df['PRC'].transform(lambda x: ewma(x, span = 12).mean())
+        df['Slow_Exp_Moving_Avg'] = df['PRC'].transform(lambda x: ewma(x, span = 26).mean())
+        df['Momentum_Factor'] = df['Fast_Exp_Moving_Avg']/df['Slow_Exp_Moving_Avg']
+
+        df['Moving_Avg'] = df['PRC'].transform(lambda x: x.rolling(window=20).mean())
+        df['Moving_Std_Deviation'] = df['PRC'].transform(lambda x: x.rolling(window=20).std())  
+        
+        df['Ultimate_Oscillator'] = UltimateOscillator(df['ASKHI'], df['BIDLO'], df['PRC'], fillna = True).uo()
+        df['RSI_Indicator'] = RSIIndicator(df['PRC'], 14, False).rsi()
+        df['Stochastic_Oscillator'] = StochasticOscillator(df['ASKHI'], df['BIDLO'], df['PRC'], 14, 3, True).stoch()
+
+        # print("3")
+        n_fast = 12
+        n_slow = 26
+        df['MACD'] = df['Slow_Exp_Moving_Avg'] - df['Fast_Exp_Moving_Avg']
+
+        # Bollinger Bands
+        df['BollingerB_UP'] =  df['Moving_Avg'] + df['Moving_Std_Deviation']*2
+        df['BollingerB_DOWN'] = df['Moving_Avg'] - df['Moving_Std_Deviation']*2
+
         df.dropna(inplace = True)
         X = df[df.columns[~df.columns.isin(['1_day_return','TICKER'])]]
         y = df.loc[:, '1_day_return']
+
     except:
         return jsonify({})
 
+
+    
     xgb = XGBClassifier()
-    xgb.fit(X, y)
+    xgb.fit(X.iloc[:-1], y.iloc[:-1])
 
     predict_for = pd.DataFrame(X.iloc[date]).T
+
+    # print(xgb.predict(X))
 
 
     answer = xgb.predict_proba(predict_for)[0]
@@ -53,7 +88,7 @@ def get_stock_predictions():
     confidence = max(answer) * 100
     
     print('Predicted Label:', prediction)
-    print('Confidence:', confidence * 100)
+    print('Confidence:', confidence)
 
     results = {
         'prediction' : str(prediction),
