e9e57484bfdfcc3281ba7071a4c6e09ae53d2c77
welvin21 <wewe2712@connect.hku.hk>
2020-10-29 18:22:28 +0800

    serer: enable get news headline link endpoint

--- server/app.py
+++ server/app.py
@@ -14,7 +14,7 @@ import routes.get_returns_data
 import routes.get_diversify_recommendations
 import routes.get_news_sentiments
 import routes.get_stock_data_by_industry
-
+import routes.get_news_headline_link
 
 @app.route('/')
 @cross_origin(supports_credentials=True)
