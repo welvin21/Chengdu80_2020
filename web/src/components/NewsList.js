5ec79965b1d230fb2d951e5b8b60e02520d78cbc
agarwalmanya <manya18ag@gmail.com>
2020-10-29 17:44:30 +0800

    web: add news list to interpretation

---
+++ web/src/components/NewsList.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+import { List, Typography, Divider } from "antd";
+
+export const NewsList = ({ ticker }) => {
+  const [data, setData] = useState([]);
+  return <div></div>;
+};
