2b9987f9bdecea6c918f293a14ec8fe270649a3d
welvin21 <47080921+welvin21@users.noreply.github.com>
2020-10-29 14:27:40 +0800

    web: add portfolio context (#25)

--- web/src/features/Dashboard.js
+++ web/src/features/Dashboard.js
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Row, Col, Card } from "antd";
 import { Portfolio } from "../components/Portfolio";
 import { RecommendationsDiversify } from "../components/RecommendationsDiversify";
 import { RecommendationsLongShort } from "../components/RecommendationsLongShort";
+import { PortfolioContext } from "../context";
 
 export const Dashboard = () => {
+  const { portfolio, setPortfolio } = useContext(PortfolioContext);
+
   return (
-    <Card style={{height:"100vh", width:"80%", marginLeft:"auto", marginRight:"auto"}}>
-      <Row style={{justifyContent:"center"}}>
+    <Card
+      style={{
+        height: "100vh",
+        width: "80%",
+        marginLeft: "auto",
+        marginRight: "auto",
+      }}
+    >
+      <Row style={{ justifyContent: "center" }}>
         <Col span={24}>
           <RecommendationsDiversify />
         </Col>
       </Row>
-      <Row style={{justifyContent:"center"}}>
+      <Row style={{ justifyContent: "center" }}>
         <Col span={24}>
           <RecommendationsLongShort />
         </Col>
       </Row>
-      <Row style={{justifyContent:"center"}}>
+      <Row style={{ justifyContent: "center" }}>
         <Col span={24}>
           <Portfolio />
         </Col>
